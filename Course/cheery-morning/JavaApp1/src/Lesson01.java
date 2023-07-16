1cf170ec68bdfd95861ff5d1162166ce55e8bc9d
tonynguyenbk <truong.nd172877@sis.hust.edu.vn>
2023-07-16 16:20:02 +0700

    first commit

---
+++ Course/cheery-morning/JavaApp1/src/Lesson01.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TonyNguyen
+ */
+public class Lesson01 {
+    public static void main(String[] args) {    // psvm
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+        System.out.println(a);
+//        int a = 100;
+//        long b = 3468764316879635498L;
+//        float PI = 3.14F;
+//        double d = 3.9874654987;        
+//                     
+//        char kiTu = 'A';
+//        System.out.println("Gia tri cua a la: " + a);   //sout
+//        System.out.println(kiTu);
+//        System.out.printf("Gia tri cua PI va d la: %.2f va %.2f\n", PI, d);
+    }
+}
+// Shift + F6: run
